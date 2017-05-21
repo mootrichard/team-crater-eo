339d15cd384645b560aa1efba454c7384aca68b6
neligajb <boone@cobblehilldigital.com>
2017-05-21 15:03:39 -0400

    client route updates

--- server/controllers/clients.js
+++ server/controllers/clients.js
@@ -10,9 +10,6 @@ module.exports = {
     let formData  = req.body;
     let date = new Date();
 
-    console.log(formData.photo);
-    return;
-
     if (formData === null || formData === undefined || formData === 'undefined') {
       res.status(400).send('bad request');
       return;
