674bca965d27171569a7b865fe73cca683b29fea
Richard Moot <moot@richardmoot.com>
2017-05-28 12:54:36 -0700

    Fix url params to camelcase

--- server/routes/jobDevelopers.js
+++ server/routes/jobDevelopers.js
@@ -3,7 +3,7 @@ const jobDevelopersController = require('../controllers').jobDevelopers;
 
 // Job Developers
 router.post('/', jobDevelopersController.create);
-router.get('/:job-developer-id', jobDevelopersController.getOne);
+router.get('/:jobDeveloperId', jobDevelopersController.getOne);
 router.get('/', jobDevelopersController.getAll);
 
 module.exports = router;
--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -3,7 +3,7 @@ const jobsController = require('../controllers').jobs;
 
 // Jobs
 router.post('/', jobsController.create);
-router.get('/:job-id', jobsController.getOne);
+router.get('/:jobId', jobsController.getOne);
 router.get('/', jobsController.getAll);
 
 module.exports = router;
