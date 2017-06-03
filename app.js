cf9023dec34eb7e21e9b5de35f10673396e20ac2
neligajb <boone@cobblehilldigital.com>
2017-06-03 10:40:57 -0400

    set up file structure for styles

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(session({
   saveUninitialized: false
 }));
 app.use(passport.initialize());
-app.use(passport.session())
+app.use(passport.session());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
