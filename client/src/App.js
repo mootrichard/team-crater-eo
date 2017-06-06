f4c0b78e5d80f20fc1752770b12a134cbfbbe8e2
Richard Moot <moot@richardmoot.com>
2017-06-06 13:45:04 -0700

    Update style of login component

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,10 @@ import ClientData from './Components/ClientData.js';
 import LoginForm from './Components/LoginForm';
 import {
   BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink} from 'react-router-dom';
+  Route
+} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-
-const Home = () => (
-  <div className="container">
-    <h2>Home</h2>
-  </div>
-)
 
 class App extends Component {
   constructor(props){
@@ -46,8 +39,7 @@ class App extends Component {
         <Router>
           <div>
             <div className="container text-center">
-              <Route exact path="/" component={Home} />
-              <Route path="/login" render={(props)=>{return (<LoginForm logIn={this.logIn} />)}} />
+              <Route exact path="/" render={(props)=>{return (<LoginForm logIn={this.logIn} />)}} />
               <Route path="/masterform" component={MasterForm} />
               <Route path="/clients" component={ClientData} />
             </div>
