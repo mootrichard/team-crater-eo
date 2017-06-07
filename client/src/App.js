84ce3940e39281dc17b834466ad4d4d546732aea
Richard Moot <moot@richardmoot.com>
2017-06-06 21:03:27 -0700

    Add in login routing and a dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import MasterForm from './Components/MasterForm.js';
-import ClientData from './Components/ClientData.js';
+import Dashboard from './Components/Dashboard';
 import LoginForm from './Components/LoginForm';
 import {
   BrowserRouter as Router,
@@ -11,27 +10,6 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      loggedIn: false
-    };
-
-    this.logIn = this.logIn.bind(this);
-  }
-
-  logIn = (token)=>{
-    if(token){
-        this.setState({
-            loggedIn: true
-        })
-    }
-  };
-
-  componentWillMount = ()=>{
-    this.logIn(localStorage.getItem('token'))
-  };
-
 
   render() {
     return (
@@ -39,9 +17,8 @@ class App extends Component {
         <Router>
           <div>
             <div className="container text-center">
-              <Route exact path="/" render={(props)=>{return (<LoginForm logIn={this.logIn} />)}} />
-              <Route path="/masterform" component={MasterForm} />
-              <Route path="/clients" component={ClientData} />
+              <Route exact path="/" component={LoginForm} />
+              <Route path="/dashboard" component={Dashboard} />
             </div>
           </div>
         </Router>
