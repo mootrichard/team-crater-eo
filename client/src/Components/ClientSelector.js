17d49365fda4ed002b8176e14887fb38717e7ee9
Richard Moot <moot@richardmoot.com>
2017-06-07 17:48:19 -0700

    Add in login and logout components

--- client/src/Components/ClientSelector.js
+++ client/src/Components/ClientSelector.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Field} from 'redux-form'
 
-let ClientData = () => {
+class ClientData extends Component {
+  constructor(props){
+    super(props)
+    this.populateSelect = this.populateSelect.bind(this);
+    this.getClients = this.getClients.bind(this);
+    this.state = {
+      clientData: {}
+    }
+  }
 
-  const populateSelect = (x) => {
+  componentDidMount(){
+    this.getClients();
+  }
+
+  populateSelect (x){
     for (var index in x) {
       var node = document.createElement('option');
       node.text = x[index].first_name + ' ' + x[index].last_name;
       node.value = x[index].id;
       document.getElementById("clientselect").appendChild(node);
     }
-  };
+  }
 
-  const getClients = () => {
+  getClients(){
     fetch("/api/clients", {
       headers: {
         'Accept': 'application/json',
@@ -25,19 +37,19 @@ let ClientData = () => {
     .then(json => {
       var json_data = JSON.stringify(json);
       var json_object = JSON.parse(json_data);
-      populateSelect(json_object);
+      this.populateSelect(json_object);
     });
-  };
+  }
 
-  return (
-    <div>
-      <Field name="clientselect" id="clientselect" component="select">
-        <option></option>
-        {getClients()}
-      </Field>
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <Field name="clientselect" id="clientselect" component="select">
+          <option></option>
+        </Field>
+      </div>
+    );
+  }
 }
-
 
   export default (ClientData);
